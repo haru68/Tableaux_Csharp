cbc10259bdd28f0911044786bcc137884aa57cf6
haru68 <58692041+haru68@users.noreply.github.com>
2020-01-06 17:27:04 +0100

    Update Tableaux.cs

--- Tableaux.cs
+++ Tableaux.cs
@@ -1,17 +1,20 @@
 using System;
 
-class MainClass {
-  public static void Main (string[] args) {
-    int[] FirstArray = new int[3] { 1, 2, 3 };
-    Console.WriteLine(FirstArray);
-    int[] SecondArray = new int[3] { 4, 5, 6 };
-    Console.WriteLine(SecondArray);
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int[] FirstArray = new int[3] { 1, 2, 3 };
+        Console.WriteLine("Premier tableau:");
+        Console.WriteLine("[{0}]", string.Join(", ", FirstArray));
 
-    int[][] ThirdArray = new int[3][];
-        ThirdArray[1] = FirstArray;
-        ThirdArray[2] = SecondArray;
-        
-      
-    Console.WriteLine(ThirdArray);
-  }
+        int[] SecondArray = new int[3] { 4, 5, 6 };
+        Console.WriteLine("Second tableau:");
+        Console.WriteLine("[{0}]", string.Join(", ", SecondArray));
+
+        int[] ThirdArray = new int[6] { 1, 2, 3, 4, 5, 6 };
+
+        Console.WriteLine("Troisième tableau:");
+        Console.WriteLine("[{0}]", string.Join(", ", ThirdArray));
+    }
 }
